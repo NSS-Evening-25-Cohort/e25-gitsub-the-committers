5678e28d15643228e62d948017a375023dbff4b4
Abigail-DePriest <abby.depriest@gmail.com>
2023-11-12 21:58:56 -0600

    got create repository button working

--- repos.js
+++ repos.js
@@ -1,31 +1,37 @@
 const repos = [
   {
+    id: 1,
     title: "King of Memphis",
     textContent: "Aenean sit amet nulla sed magna ullamcorper vestibulum" 
 
   },
-  {
+  { 
+    id: 2,
     title: "Dum and Dummer",
     textContent: "Etiam congue ex vel arcu vehicula hendrerit sed at risus"
 
   },
 
   {
+    id: 3,
     title: "Bulletproof",
     textContent: "Pellentesque dignissim ante at eros hendrerit, nec porta quam imperdiet"
   },
   {
+    id: 4,
     title: "Thinking Out Loud",
     textContent: "Etiam vitae magna non lacus laoreet gravida sit amet quis nibh"
   },
   {
+    
+  id: 5,
   title: "Gelato",
   textContent: "Phasellus in sapien consectetur, tempus libero eget, euismod metus."
   }
-]
+];
 
 
-const dataDiv = document.querySelector("#repoCards");
+//const dataDiv = document.querySelector("#repoCards");
 
 
 
@@ -33,52 +39,39 @@ const cardsOnDom = (repos) => {
   let domString ="";
 
   for (let i= 0; i < repos.length; i++) {
-    domString+= `<div class="card" style="width: 18rem;">
+    domString+= `
+    <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${repos[i].title}</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
       <p class="card-text">${repos[i].textContent}</p>
     </div>
-  </div>`
+  </div>`;
   }
 
-
-  dataDiv.innerHTML = domString;
+ const repoCards = document.querySelector("#repoCards");
+  repoCards.innerHTML = domString;
   
 };
 
-cardsOnDom(repos);
 
 
-const form = document.querySelector("form");
 
-const createNewRepo = (e) => {
+
+const createRepository = (e) => {
   e.preventDefault();
 
   const newRepo = {
-  title: document.querySelector("#title").value,
-  textContent: document.querySelector("#textContent").value,
+  id: repos.length + 1,
+  title: document.querySelector("#repo-name").value,
+  textContent: document.querySelector("#repo-description").value,
 }
 
 repos.push(newRepo);
 cardsOnDom(repos);
-form.reset();
+document.querySelector("form").reset();
 
 }
 
-form.addEventListener("submit", createNewRepo);
-
-
-/*const form = document.querySelector("form")
-form.addEventListener ("submit", (event) => {
-  event.preventDefault();
-  
-  const newRepo = {
-    title: document.querySelector("#title").value,
-    textContent: document.querySelector("#textContent").value,
-  };
-
-  repos.push(newRepo);
-  cardsOnDom(repos);
-  form.reset();
-})*/
+cardsOnDom(repos);
+document.querySelector("#create-repo-btn").addEventListener("click", createRepository);
